b9c1bf2fc5476e2e94eaf1246cc7f0fbfa76b71f
liuchang <913778993@qq.com>
2022-11-30 17:12:53 +0100

    add change index

--- src/components/GraphDisplay.vue
+++ src/components/GraphDisplay.vue
@@ -1,5 +1,17 @@
 <template>
-    <select v-model="selectedfield" :style="{height: 50 + 'px'}">
+    <form enctype="multipart/form-data" method="POST" action="/upload">
+        <input type="text" name="imgName" />
+        <input type="file" name="imgFile" />
+        <input type="submit" value="upload" />
+    </form>
+    <br>
+
+    <select v-model="selected_index" :style="{ height: 50 + 'px' }">
+        <option v-for="i in index" v-bind:value="i">
+            {{ i }}
+        </option>
+    </select>
+    <select v-model="selected_field" :style="{ height: 50 + 'px' }">
         <option v-for="field in fields" v-bind:value="field">
             {{ field }}
         </option>
@@ -18,12 +30,14 @@ export default {
     props: {},
     data() {
         return {
-            selectedfield: '',
+            selected_index: '',
+            selected_field: '',
+            index: [],
             fields: [],
             chartData: {
                 labels: [],
-                datasets: [{ 
-                    data: [], 
+                datasets: [{
+                    data: [],
                     backgroundColor: 'rgb(255, 99, 132)',
                     label: ''
                 }]
@@ -32,7 +46,7 @@ export default {
     },
     computed: {},
     watch: {
-        selectedfield(newSelectedField, oldSelectedField) {
+        selected_field(newSelectedField, oldSelectedField) {
             axios({
                 method: 'post',
                 url: 'http://127.0.0.1:8080/aggregation',
@@ -48,6 +62,19 @@ export default {
             }).catch(err => {
                 console.log(err)
             })
+        },
+        selected_index(newSelectedIndex, oldSelectedField) {
+            console.log(newSelectedIndex)
+            axios({
+                method: 'post',
+                url: 'http://127.0.0.1:8080/field',
+                headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+                data: newSelectedIndex
+            }).then(res => {
+                this.fields = res.data
+            }).catch(err => {
+                console.log(err)
+            })
         }
     },
     methods: {
@@ -56,12 +83,10 @@ export default {
     mounted() {
         document.querySelector('body').setAttribute('style', 'background:#fff')
         axios({
-            method: 'post',
-            url: 'http://127.0.0.1:8080/field',
-            headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
-            data: 'food'
+            method: 'get',
+            url: 'http://127.0.0.1:8080/index',
         }).then(res => {
-            this.fields = res.data
+            this.index = res.data
         }).catch(err => {
             console.log(err)
         })
