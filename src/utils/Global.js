9477ee7c97dcd34571d4f062acd9aa7b77413a59
larry <913778993@qq.com>
2023-02-15 15:27:15 +0100

    create an express app that can act as a backend.
    you can now switch the connection by changing the 'mode' in Global.js

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -1,5 +1,22 @@
-const fileSrc = 'http://127.0.0.1:10000';
-const querySrc = 'http://127.0.0.1:8080'
+const mode = 'f'
+
+
+
+
+let fileSrc = ''
+let querySrc = ''
+if (mode == 'f')
+{
+  fileSrc = 'http://127.0.0.1:3000'
+  querySrc = 'http://127.0.0.1:3000'
+}
+else 
+{
+  fileSrc = 'http://127.0.0.1:10000'
+  querySrc = 'http://127.0.0.1:8080'
+}
+
+
 export default
   {
     fileSrc,
